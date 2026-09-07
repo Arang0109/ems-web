a2a19703fa27789f7b47c0863a639d29f656fad3
Arang0109 <kmsq321@gmail.com>
2026-09-07 16:36:55 +0900

    오타수정 (#7)

--- src/entities/client/model/use-client-detail.ts
+++ src/entities/client/model/use-client-detail.ts
@@ -5,16 +5,11 @@ import { clientApi } from "../api/api";
 import { clientKeys } from "./query-keys";
 import type { Client } from "./types";
 
-interface Props {
-  /** null 이면 조회하지 않는다. */
-  id: number | null;
-}
-
 /** 의뢰기관 상세. 대상이 바뀌면 이전 값을 즉시 버린다. */
-export const useClientDetail = ({ id }: Props) =>
+export const useClientDetail = (clientId: number | null) =>
   useEntityQuery<Client | null>({
-    queryKey: clientKeys.detail(id as number),
-    queryFn: async () => unwrapMessage(await clientApi.getClient(id as number)),
+    queryKey: clientKeys.detail(clientId as number),
+    queryFn: async () => unwrapMessage(await clientApi.getClient(clientId as number)),
     initialData: null,
-    enabled: id != null,
+    enabled: clientId != null,
   });
